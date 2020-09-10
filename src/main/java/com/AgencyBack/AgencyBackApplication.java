c5464385a049a33b893ff8f01f0728a2de58c0df
mathieuportal <61821501+mathieuportal@users.noreply.github.com>
2020-09-10 11:02:37 +0200

    Add files via upload

---
+++ src/main/java/com/AgencyBack/AgencyBackApplication.java
@@ -0,0 +1,13 @@
+package com.AgencyBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AgencyBack/AgencyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AgencyBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgencyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
