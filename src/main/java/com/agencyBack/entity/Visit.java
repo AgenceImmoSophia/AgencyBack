a28d8f0eec8c111fc3e4e200ffd8dc9904d70562
mathieuportal <mathieuportal06@gmail.com>
2020-09-10 12:27:44 +0200

    Creation of the Visit entity
    Visit entity created, as well as Visit Repository.

---
+++ src/main/java/com/agencyBack/entity/Visit.java
@@ -0,0 +1,51 @@
+package com.agencyBack.entity;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Visit implements Serializable{
+	
+	private Date date
+	private Good good;
+	private EstateAgent estateAgent;
+	private Client client;
+	
+	public Visit() {
+		
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Good getGood() {
+		return good;
+	}
+
+	public void setGood(Good good) {
+		this.good = good;
+	}
+
+	public EstateAgent getEstateAgent() {
+		return estateAgent;
+	}
+
+	public void setEstateAgent(EstateAgent estateAgent) {
+		this.estateAgent = estateAgent;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/agencyBack/repository/VisitRepository.java
@@ -0,0 +1,8 @@
+package com.agencyBack.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.agencyBack.entity.Visit;
+
+public interface VisitRepository extends CrudRepository<Visit,Long> {
+}
