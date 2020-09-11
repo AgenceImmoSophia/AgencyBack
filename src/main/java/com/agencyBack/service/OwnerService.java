b8f596ee3535320f4847c7ea2da7be5cf4062317
Pauline-Humbert <42661326+Pauline-Humbert@users.noreply.github.com>
2020-09-11 12:39:07 +0200

    Client Owner Service Impl

--- src/main/java/com/agencyBack/service/OwnerService.java
+++ src/main/java/com/agencyBack/service/OwnerService.java
@@ -1,19 +1,16 @@
 package com.agencyBack.service;
 
-import java.util.List;
-
 import com.agencyBack.entity.Good;
+import com.agencyBack.entity.Owner;
+
+import javassist.NotFoundException;
 
 public interface OwnerService extends UserService{
 	
-	public void findOwnedGoodsByName(String nameGood);
+	public Good findOwnedGoodsByNameFromOwnedGoods(Owner owner,String nameGood);
 	
-	public Iterable<Good> findAllOwnedGoods();
+	public void createOwnedGoodInListOwnedGood(Owner owner, Good good) throws NotFoundException;
 	
-	public void createOwnedGoodInListOwnedGood(List<Good> listGood, Good good);
-	
-	public void editOwnedGoodFromListOwnedGood(List<Good> listGood, Good good);
-	
-	public void deleteOwnedGoodFromListOwnedGood(List<Good> listGood, Good good);
+	public void deleteOwnedGoodFromListOwnedGood(Owner owner, Good good) throws NotFoundException;
 
 }
