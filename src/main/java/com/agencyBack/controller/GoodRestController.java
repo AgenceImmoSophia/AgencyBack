b722c44fd121082981132dc0627805f3a564262e
mathieuportal <mathieuportal06@gmail.com>
2020-09-21 14:50:13 +0200

    Added editGood

--- src/main/java/com/agencyBack/controller/GoodRestController.java
+++ src/main/java/com/agencyBack/controller/GoodRestController.java
@@ -87,6 +87,43 @@ public class GoodRestController {
         }
         return new ResponseEntity<>(good, HttpStatus.CREATED);
     }
+    
+    @PostMapping(value = "good", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void editGood(@RequestBody Good good, @PathVariable("id") Long id) throws NotFoundException {
+    	good.setId(id);
+        	
+	    Address addressGood = good.getAddress();	
+	    List<Address> listExistingAddress = this.addressService.getAll();
+	    		
+	    Address addressExisting = new Address();
+	    int counterAddress = 0;
+	    for ( int a = 0; a < listExistingAddress.size(); a++) {
+	    	addressExisting.setCountry(listExistingAddress.get(a).getCountry());
+	    	addressExisting.setCity(listExistingAddress.get(a).getCity());
+	    	addressExisting.setStreet(listExistingAddress.get(a).getStreet());
+	    	addressExisting.setStreetNber(listExistingAddress.get(a).getStreetNber());
+	    	addressExisting.setZipcode(listExistingAddress.get(a).getZipcode());
+	
+	    	if ( addressGood.equals(addressExisting) ) {
+	    		good.setAddress(listExistingAddress.get(a));
+	    		this.goodService.generateCode(good);
+	    		this.goodService.create(good);
+	    	    return;
+	    	}
+	    			
+	    	counterAddress = a+1;
+	    }
+	    		
+	    if ( counterAddress >= listExistingAddress.size()) {
+	    	addressGood.setId(null);
+	    	good.setAddress(addressGood);
+	    	this.addressService.create(addressGood);
+	    	this.goodService.generateCode(good);
+	    	this.goodService.create(good);
+	    	   return;
+	    }   
+    }
+
 
     @DeleteMapping (value = "/good/{id}") // Can't delete if linked with owner, listDesiredGood, Visit or Client
     public ResponseEntity<Object> deleteGood(@PathVariable("id") Long id) throws NotFoundException {
